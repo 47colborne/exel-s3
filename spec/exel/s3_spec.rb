b52cd7eec0560b0652eb25ac7c5987f162f48177
David <david@retailcommon.com>
2015-12-15 10:46:24 -0500

    Pending test now passes with updated EXEL

--- spec/exel/s3_spec.rb
+++ spec/exel/s3_spec.rb
@@ -1,6 +1,6 @@
 module EXEL
   describe S3 do
-    xit 'sets itself as the remote provider' do
+    it 'sets itself as the remote provider' do
       expect(EXEL.remote_provider).to eq(EXEL::S3::S3Provider)
     end
   end
